bdd619b23cdaa5cafb3a57d0f503b3fc78f07753
Hà Nguyễn Thanh <hant2@rabiloo.com>
2023-06-24 16:32:14 +0700

    add draggable droppable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import initialData from "./initial-data";
+import Column from "./column";
+import { DragDropContext } from "react-beautiful-dnd";
+export default class App extends React.Component {
+    state = initialData;
+    onDragEnd() {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    }
+    render() {
+        return (
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                {this.state.columnOrder.map((columnId) => {
+                    const column = this.state.columns[columnId];
+                    const tasks = column.taskIds.map(
+                        (taskId) => this.state.tasks[taskId]
+                    );
+
+                    return (
+                        <Column key={column.id} column={column} tasks={tasks} />
+                    );
+                })}
+            </DragDropContext>
+        );
+    }
 }
-
-export default App;
